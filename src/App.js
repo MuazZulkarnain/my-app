86a30a3f813f8504e4f82d7946447c33d6c2bc62
Muaz Zul <idmuazzulkarnain@gmail.com>
2023-01-30 13:33:43 +0800

    Updating the screen layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [user, setUser] = useState("John Doe");
+  const [assetId, setAssetId] = useState("");
+  const [owner, setOwner] = useState("");
+  const [inputAssetId, setInputAssetId] = useState("");
+  const [inputAmount, setInputAmount] = useState(0);
+  const [isTokenizing, setIsTokenizing] = useState(true);
+
+  const handleTokenize = () => {
+    // Perform the tokenization process here.
+    setAssetId("ABC123");
+    setOwner(user);
+  };
+
+  const handleQuery = () => {
+    // Query the asset information here.
+    setAssetId(inputAssetId);
+    setOwner("Jane Doe");
+  };
+
+  const handleTransfer = () => {
+    // Perform the transfer process here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <h1>Welcome, {user}!</h1>
+      {isTokenizing ? (
+        <>
+          <text>Tokenize your carbon credit:</text>
+          <input
+            type="number"
+            placeholder="Enter amount"
+            value={inputAmount}
+            onChange={(e) => setInputAmount(e.target.value)}
+          />
+          <button onClick={handleTokenize} className="tokenize-button">
+            Tokenize
+          </button>
+        </>
+      ) : (
+        <>
+          <text>Query your carbon credit:</text>
+          <input
+            type="text"
+            placeholder="Enter asset ID"
+            value={inputAssetId}
+            onChange={(e) => setInputAssetId(e.target.value)}
+          />
+          <button type="submit" onClick={handleQuery} className="query-button">
+            Query
+          </button>
+        </>
+      )}
+      <hr />
+      <text>Asset ID: {assetId}</text>
+      <text>Owner: {owner}</text>
+      <>
+        <button
+          onClick={() => setIsTokenizing(!isTokenizing)}
+          className="toggle-button"
         >
-          Learn React
-        </a>
-      </header>
+          {isTokenizing ? "Query" : "Tokenize"}
+        </button>
+        {!isTokenizing && assetId !== "" ? (
+          <button onClick={handleTransfer} className="transfer-button">
+            Transfer
+          </button>
+        ) : null}
+      </>
     </div>
   );
 }
