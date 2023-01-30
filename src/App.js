8989d4ab80d9a4a25617bd9ea371f73144d06978
Muaz Zul <idmuazzulkarnain@gmail.com>
2023-01-30 14:18:02 +0800

    Added sign in and sign out

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState("User X");
+  const [user, setUser] = useState("");
+  const [loggedIn, setLoggedIn] = useState(false);
   const [assetId, setAssetId] = useState("");
   const [owner, setOwner] = useState("");
   const [inputAssetId, setInputAssetId] = useState("");
   const [inputAmount, setInputAmount] = useState(0);
-  const [inputRecipient, setInputRecipient] = useState("");
+  const [inputRecipientId, setInputRecipientId] = useState("");
   const [activePage, setActivePage] = useState("Tokenize");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSignedIn, setIsSignedIn] = useState(false);
 
   const handleTokenize = () => {
     // Perform the tokenization process here.
@@ -25,92 +29,131 @@ function App() {
   const handleTransfer = () => {
     // Transfer the asset here.
     setAssetId(inputAssetId);
-    setOwner(inputRecipient);
+    setOwner(inputRecipientId);
+  };
+
+  const handleSignIn = () => {
+    setUser("User X");
+    setLoggedIn(true);
+  };
+
+  const handleSignOut = () => {
+    setUser("");
+    setLoggedIn(false);
   };
 
   return (
     <div className="container">
-      <h1>Welcome, {user}!</h1>
-      <div className="side-nav">
-        <button
-          className={`side-nav-button ${
-            activePage === "Tokenize" ? "active" : ""
-          }`}
-          onClick={() => setActivePage("Tokenize")}
-        >
-          Tokenize
-        </button>
-        <button
-          className={`side-nav-button ${
-            activePage === "Query" ? "active" : ""
-          }`}
-          onClick={() => setActivePage("Query")}
-        >
-          Query
-        </button>
-        <button
-          className={`side-nav-button ${
-            activePage === "Transfer" ? "active" : ""
-          }`}
-          onClick={() => setActivePage("Transfer")}
-        >
-          Transfer
-        </button>
-      </div>
-      {activePage === "Tokenize" && (
-        <div className="input-form">
-          <text>Tokenize your carbon credit:</text>
-          <input
-            type="number"
-            placeholder="Enter amount"
-            value={inputAmount}
-            onChange={(e) => setInputAmount(e.target.value)}
-          />
-          <button onClick={handleTokenize} className="tokenize-button">
-            Tokenize
+      {!loggedIn ? (
+        <div className="sign-in">
+          <text>Please sign in to continue:</text>
+          <button onClick={handleSignIn} className="sign-in-button">
+            Sign In
           </button>
         </div>
-      )}
-      {activePage === "Query" && (
-        <div className="input-form">
-          <text>Enter asset ID to query:</text>
-          <input
-            type="text"
-            placeholder="Enter asset ID"
-            value={inputAssetId}
-            onChange={(e) => setInputAssetId(e.target.value)}
-          />
-          <button onClick={handleQuery} className="query-button">
-            Query
-          </button>
-        </div>
-      )}
-      {activePage === "Transfer" && (
-        <div className="input-form">
-          <text>Enter asset ID and recipient ID to transfer:</text>
-          <input
-            type="text"
-            placeholder="Enter asset ID"
-            value={inputAssetId}
-            onChange={(e) => setInputAssetId(e.target.value)}
-          />
-          <input
-            type="text"
-            placeholder="Enter recipient ID"
-            value={inputRecipient}
-            onChange={(e) => setInputRecipient(e.target.value)}
-          />
-          <button onClick={handleTransfer} className="transfer-button">
-            Transfer
-          </button>
-        </div>
-      )}
-
-      {assetId && (
-        <div className="asset-info">
-          <text>Asset ID: {assetId}</text>
-          <text>Owner: {owner}</text>
-        </div>
+      ) : (
+        <>
+          <h1>Welcome, {user}!</h1>
+          <div className="side-nav">
+            <button
+              className={`side-nav-button ${
+                activePage === "Tokenize" ? "active" : ""
+              }`}
+              onClick={() => setActivePage("Tokenize")}
+            >
+              Tokenize
+            </button>
+            <button
+              className={`side-nav-button ${
+                activePage === "Query" ? "active" : ""
+              }`}
+              onClick={() => setActivePage("Query")}
+            >
+              Query
+            </button>
+            <button
+              className={`side-nav-button ${
+                activePage === "Transfer" ? "active" : ""
+              }`}
+              onClick={() => setActivePage("Transfer")}
+            >
+              Transfer
+            </button>
+            <button onClick={handleSignOut} className="sign-out-button">
+              Sign Out
+            </button>
+          </div>
+          {activePage === "Tokenize" && (
+            <div className="input-form">
+              <text>Tokenize your carbon credit:</text>
+              <input
+                type="number"
+                placeholder="Enter amount"
+                value={inputAmount}
+                onChange={(e) => setInputAmount(e.target.value)}
+                className="input-field"
+              />
+              <button onClick={handleTokenize} className="submit-button">
+                Tokenize
+              </button>
+              <text>
+                Asset ID: <span className="result">{assetId}</span>
+              </text>
+              <text>
+                Owner: <span className="result">{owner}</span>
+              </text>
+            </div>
+          )}
+          {activePage === "Query" && (
+            <div className="input-form">
+              <text>Query your carbon credit information:</text>
+              <input
+                type="text"
+                placeholder="Enter asset ID"
+                value={inputAssetId}
+                onChange={(e) => setInputAssetId(e.target.value)}
+                className="input-field"
+              />
+              <button onClick={handleQuery} className="submit-button">
+                Query
+              </button>
+              <text>
+                Asset ID: <span className="result">{assetId}</span>
+              </text>
+              <text>
+                Owner: <span className="result">{owner}</span>
+              </text>
+            </div>
+          )}
+          {activePage === "Transfer" && (
+            <div className="input-form">
+              <text>Transfer your carbon credit:</text>
+              <input
+                type="text"
+                placeholder="Enter asset ID"
+                value={inputAssetId}
+                onChange={(e) => setInputAssetId(e.target.value)}
+                className="input-field"
+              />
+              <input
+                type="text"
+                placeholder="Enter recipient ID"
+                value={inputRecipientId}
+                onChange={(e) => setInputRecipientId(e.target.value)}
+                className="input-field"
+              />
+              <button onClick={handleTransfer} className="submit-button">
+                Transfer
+              </button>
+              <text>
+                Asset ID: <span className="result">{assetId}</span>
+              </text>
+              <text>
+                Owner: <span className="result">{owner}</span>
+              </text>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
